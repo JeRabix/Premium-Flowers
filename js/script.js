421a7498cc1558834aa7964cdc841cf84e732441
YURCHENKO EVGENIY <rabix346@yandex.ru>
2019-07-29 15:48:21 +0300

    мой

--- js/script.js
+++ js/script.js
@@ -32,12 +32,18 @@ const targetElement = document.querySelector(".header-mobile__menu");
 
 
 //tabs
+if (window.innerWidth < 1280) {
+
+}
+else {
 	$.ionTabs("#tabs_1", {
-    type: "storage", // hash, storage или none
-});
+		type: "storage", // hash, storage или none
+	});
+}
 
 
-//card-slider
+
+//card-slider действие при добавлении в корзину в мобилке поп-ап в пк корзина справа выезжает
 $('.card-slider__list').find('img').click(function (e) {
 	e.preventDefault();
 	var thes = $(this);
@@ -48,7 +54,41 @@ $('.card-slider__list').find('img').click(function (e) {
 	$(this).parent().siblings('.card-slider__current').find('img').fadeIn('200');
 });
 
+//cart
+$('.button').on('click', function(event) {
+	event.preventDefault();
+	if (window.innerWidth < 1280) {
+		$('.modal-bg').fadeIn();
+		$('#modalAddToCart').fadeIn();
 
+	}
+	else {
+		$('.main-cart').css('transform', 'translateX(0)');
+	}
+});
+
+$('.modal-close').click(function(event) {
+	$('.modal-bg').fadeOut();
+	$('.modal-add').fadeOut();
+});
+
+
+//mobile slider sales on index
+if (window.innerWidth < 1280) {
+	$('.ycont--sales').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		 // autoplay: true,
+		 // infinite: false,
+	});
+}
+
+//card карточка товара в мобилке открытие пунктов
+$('.card-mobile-list__item').click(function(event) {
+	$(this).next().slideToggle();
+	$(this).next().find('span').toggleClass('active');
+});
 
 
 
